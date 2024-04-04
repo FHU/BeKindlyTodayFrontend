193c698e44f2de5fbf97d1d672ec30a02cc382fd
Priscilla Harris <89226780+PHarris2002@users.noreply.github.com>
2024-04-04 15:15:20 -0500

    Progress on PostPage

--- src/PostPage.tsx
+++ src/PostPage.tsx
@@ -1,22 +1,54 @@
 import React from 'react';
+import './index.css';
 import './PostPage.css';
+import { CiCalendar } from "react-icons/ci";
 
 interface Post {
-  title: string;
-  content: string;
+  challenge: string;
 }
 
 const PostPage: React.FC = () => {
   // Define a static post
   const post: Post = {
-    title: "Sample Post",
-    content: "This is a sample post content. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac justo ac ipsum convallis rutrum."
+    challenge: "Send a text to a loved one to show your appreciation."
   };
 
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
+    // Top-Parent Container
+    <div className="flex flex-col gap-10 justify-between items-center">
+        {/* Header */}
+        <div className="flex w-full h-24 text-2xl justify-between items-center" style={{ backgroundColor: '#2485A9', color: '#ffffff' }}>
+            <img src={'src/assets/logo.png'} alt="Centered Image" style={{ width: '65px', height: '65px' }} />
+            <h1>BeKindly</h1>
+            <div className="flex items-center">
+                <div className="text-6xl text-black"><CiCalendar /></div>
+                <div className="rounded-full bg-slate-100 p-7"></div>
+            </div>
+        </div>
+
+        {/* Timer */}
+        <div className="border-2 px-10 py-2 border-black rounded-lg font-bold">
+            <h2 className="text-2xl">00:00:00</h2>
+        </div>
+
+        {/* Challenge */}
+        <div>
+            <h2 className="text-2xl">Challenge</h2>
+            <p>{post.challenge}</p>
+        </div>
+
+        {/* Experience */}
+        <div>
+            <form action="#" method="post" className="flex flex-col">
+                <label className="text-2xl" form="experience">Experience</label>
+                <textarea className="border-2 border-black rounded-lg p-2 w-80 h-40" id="free-response" name="free-response"></textarea>
+            </form>
+        </div>
+
+        {/* Post */}
+        <div className="px-11 py-4 rounded-lg" style={{ backgroundColor: '#2485A9', color: '#ffffff' }}>
+            <h2>Post</h2>
+        </div>
     </div>
   );
 };
