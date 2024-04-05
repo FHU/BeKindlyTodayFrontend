a842c608f49363cb2a9bbf1e91ba33536f6181f0
Priscilla Harris <89226780+PHarris2002@users.noreply.github.com>
2024-04-05 09:02:55 -0500

    Progress on PostPage

--- src/PostPage.tsx
+++ src/PostPage.tsx
@@ -15,9 +15,9 @@ const PostPage: React.FC = () => {
 
   return (
     // Top-Parent Container
-    <div className="flex flex-col gap-10 justify-between items-center">
+    <div className="flex flex-col gap-y-10 items-center">
         {/* Header */}
-        <div className="flex w-full h-24 text-2xl justify-between items-center" style={{ backgroundColor: '#2485A9', color: '#ffffff' }}>
+        <div className="flex w-full h-24 text-2xl px-5 justify-between items-center" style={{ backgroundColor: '#2485A9', color: '#ffffff' }}>
             <img src={'src/assets/logo.png'} alt="Centered Image" style={{ width: '65px', height: '65px' }} />
             <h1>BeKindly</h1>
             <div className="flex items-center">
@@ -26,30 +26,33 @@ const PostPage: React.FC = () => {
             </div>
         </div>
 
+        {/* Main */}
+        <div className="flex flex-col items-center gap-4">
         {/* Timer */}
-        <div className="border-2 px-10 py-2 border-black rounded-lg font-bold">
-            <h2 className="text-2xl">00:00:00</h2>
-        </div>
+            <div className="flex justify-center border-2 w-40 py-2 border-black rounded-lg font-bold">
+                <h2 className="text-2xl">00:00:00</h2>
+            </div>
 
-        {/* Challenge */}
-        <div>
-            <h2 className="text-2xl">Challenge</h2>
-            <p>{post.challenge}</p>
-        </div>
+            {/* Challenge */}
+            <h2 className="text-2xl self-start">Challenge</h2>
+            <div className="p-6 mb-10" style={{ backgroundColor: '#2485A9', color: '#ffffff' }}>
+                <p>{post.challenge}</p>
+            </div>
 
-        {/* Experience */}
-        <div>
-            <form action="#" method="post" className="flex flex-col">
-                <label className="text-2xl" form="experience">Experience</label>
-                <textarea className="border-2 border-black rounded-lg p-2 w-80 h-40" id="free-response" name="free-response"></textarea>
-            </form>
-        </div>
+            {/* Experience */}
+            <h2 className="text-2xl self-start">Experience</h2>
+            <div className="mb-20">
+                <form action="#" method="post" className="flex flex-col">
+                        <textarea className="w-96 border-2 border-black rounded-lg p-2 h-40"></textarea>
+                </form>
+            </div>
 
-        {/* Post */}
-        <div className="px-11 py-4 rounded-lg" style={{ backgroundColor: '#2485A9', color: '#ffffff' }}>
-            <h2>Post</h2>
+            {/* Post */}
+            <div className="px-11 py-4 rounded-lg" style={{ backgroundColor: '#2485A9', color: '#ffffff' }}>
+                <h2>Post</h2>
+            </div>
         </div>
-    </div>
+        </div>
   );
 };
 
