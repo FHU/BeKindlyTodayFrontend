0df572605847b10a09f319893252ebf8970d8032
AveryH6 <89232897+AveryH6@users.noreply.github.com>
2024-04-08 14:57:10 -0500

    option.css deleted (sorry)

--- src/views/Option.tsx
+++ src/views/Option.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import 'daisyui/dist/full.css';
-import './Option.css';
 import { motion } from "framer-motion";
 
 const Option: React.FC = () => {
@@ -11,7 +10,7 @@ const Option: React.FC = () => {
         <div className='max-w-md mx-auto mt-10 p-4 font-comfortaa flex flex-col justify-center'>
             {/* bekindly */}
             <motion.div 
-            className='title flex justify-center text-5xl m-2 p-5'
+            className='option-title flex justify-center text-5xl m-2 p-5'
             initial={{ opacity:0 }}
             animate={{ opacity:1 }}
             transition={{ type: "spring", delay:2, stiffness: 260, damping: 200 }} 
@@ -22,7 +21,7 @@ const Option: React.FC = () => {
             <motion.img 
             src="src/assets/bekindlylogo.svg" 
             alt="BeKindlyLogo"
-            className="container p-2"
+            className="option-container p-2"
             initial={{ y:600, scale: 0 }}
             animate={{ y:0, rotate: 360, scale: 1 }}
             transition={{ type: "spring", stiffness: 260, velocity:50, damping: 100 }} 
@@ -30,7 +29,7 @@ const Option: React.FC = () => {
             
             {/* today */}
             <motion.div 
-            className='title flex justify-center text-5xl m-2 p-5'
+            className='option-title flex justify-center text-5xl m-2 p-5'
             initial={{ opacity:0 }}
             animate={{ opacity:1 }}
             transition={{ type: "spring", delay:2, stiffness: 260, damping: 200 }} 
@@ -47,7 +46,7 @@ const Option: React.FC = () => {
                Login
             </motion.Link> */}
             <motion.div
-                className="login-btn btn-block flex justify-center align-middle rounded-full text-xl bg-[#227C9D] text-white border-none transition-colors duration-300 hover:bg-[#1C6F87]" 
+                className="option-btn btn-block flex justify-center align-middle rounded-full text-xl bg-[#227C9D] text-white border-none transition-colors duration-300 hover:bg-[#1C6F87]" 
                 initial={{ opacity:0 }}
                 animate={{ opacity:1 }}
                 transition={{ type: "spring", delay:2.8, stiffness: 260, damping: 200 }}>
