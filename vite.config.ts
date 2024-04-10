a801f08f0b89c89b9e618c9624a1bece3bd95bf3
Evan <evandvance@gmail.com>
2024-04-10 15:27:22 -0500

    Edited vite config to be compatible with docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const FRONTEND_PORT: number = parseInt(process.env.FRONTEND_PORT || '3000'); //Parsed for type safty in config types
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-})
+  preview: {
+    port: FRONTEND_PORT,
+    strictPort: true,
+  },
+  server: {
+    port: FRONTEND_PORT,
+    strictPort: true,
+    host: true,
+    origin: `http://0.0.0.0:${FRONTEND_PORT}`,
+  },
+});
