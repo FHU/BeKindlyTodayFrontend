7e394d95bbbd5f3e1d9af6693632ab8ee6b994ef
AddisonAdcock <addison.adcock@students.fhu.edu>
2024-04-13 19:43:02 -0500

    Profile Page Updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import Home from "./views/Home"
 import Completion from "./views/Completion"
 import Calendar from "./views/Calendar"
 import Confirmation from "./views/Confirmation";
+import Profile from "./views/Profile";
 //import Main from "./Index"
 
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-
 const router = createBrowserRouter([
   {
     path: "/",
@@ -33,7 +33,11 @@ const router = createBrowserRouter([
     path: "/confirmation",
     element: <Confirmation/>,
   }
-
+  ,
+  {
+    path: "/profile",
+    element: <Profile/>,
+  }
 ]);
 
 export default function App() {
