18ba2c9893e0ef97355a46acc40f60e78d677e2b
Evan <evandvance@gmail.com>
2024-04-16 14:19:53 -0500

    Removed TS errors so the docker image would build

--- src/components/ProfileBubble.tsx
+++ src/components/ProfileBubble.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-
 const ProfileBubble = () => {
   return (
     <div className="flex items-center space-x-2 ">
-      <div className="w-12 h-12 rounded-full bg-gray-300"></div> {/* Placeholder for profile picture */}
+      <div className="w-12 h-12 rounded-full bg-gray-300"></div>{' '}
+      {/* Placeholder for profile picture */}
       <div className="bg-gray-300 px-12 py-1 rounded-lg">
-        <p className="text-xl text-gray-700">Tucker</p> {/* Placeholder for name tag */}
+        <p className="text-xl text-gray-700">Tucker</p>{' '}
+        {/* Placeholder for name tag */}
       </div>
     </div>
   );
