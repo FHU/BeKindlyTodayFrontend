1c5ffb1dda1bdfab117ef5ab6642edbaf6a73f05
Evan <evandvance@gmail.com>
2024-04-22 15:30:05 -0500

    Navbar Cleanup

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,114 +1,64 @@
-// components/Navbar.js or components/Navbar.jsx
 import { Link } from 'react-router-dom';
-//import React from 'react';
 import { BsCalendar4, BsPersonCircle } from 'react-icons/bs';
-
-//import { Link } from 'react-router-dom'; // Import Link from react-router-dom if you're using routing
 import { useKindeAuth } from '@kinde-oss/kinde-auth-react';
 
-function Navbar() {
-  const inDev = import.meta.env.VITE_ENVIRONMENT;
+const Navbar = () => {
+  const inDev = import.meta.env.VITE_ENVIRONMENT === 'dev';
 
-  if (inDev !== 'dev') {
-    const { login, register, isAuthenticated } = useKindeAuth();
-    return isAuthenticated ? (
-      <nav
-        className="flex w-full h-24 text-2xl px-5 justify-between items-center bg-kindly-blue"
-        style={{color: '#ffffff' }}
+  const { login, register, isAuthenticated } = useKindeAuth();
+
+  return (
+    <nav
+      className="flex w-full h-24 text-2xl px-5 justify-between items-center"
+      style={{ backgroundColor: '#2485A9', color: '#ffffff' }}
+    >
+      <Link
+        to="/home"
+        className="inline-block border-none hover:bg-transparent p-2 rounded-lg"
       >
-        <Link
-          to="/home"
-          className="inline-block border-none hover:bg-transparent p-2 rounded-lg"
-        >
-          <img
-            className="border-none hover:opacity-75"
-            style={{ width: '80px', height: '80px' }}
-            src={'assets/logo.png'}
-            alt="logo"
-          />
-        </Link>
-        <h1 className="text-5xl">BeKindly</h1>
-        <div className="flex items-center">
-          <Link to="/calendar" data-testid="calendar-link">
-            <div className="btn text-5xl text-white pr-4 pt-2 bg-transparent border-hidden hover:bg-transparent hover:opacity-75">
-              <BsCalendar4 />
-            </div>
-          </Link>
-          <Link to="/profile" data-testid="profile-link">
-            <div className="btn text-5xl text-white pt-2 bg-transparent border-hidden hover:bg-transparent hover:opacity-75">
-              <BsPersonCircle />
-            </div>
-          </Link>
-        </div>
-      </nav>
-    ) : (
-      <nav
-        className="flex w-full h-24 text-2xl px-5 justify-between items-center bg-kindly-blue"
-        style={{color: '#ffffff' }}
-      >
-        <Link
-          to="/home"
-          className="inline-block border-none hover:bg-transparent p-2 rounded-lg"
-        >
-          <img
-            className="border-none hover:opacity-75"
-            style={{ width: '80px', height: '80px' }}
-            src={'assets/logo.png'}
-            alt="logo"
-          />
-        </Link>
-        <h1 className="text-5xl ml-20">BeKindly</h1>
-        <div className="flex items-center">
-          <button
-            onClick={() => login()}
-            type="button"
-            className="btn text-xl text-white pt-2 bg-transparent border-hidden hover:bg-transparent hover:opacity-75"
-          >
-            Sign In
-          </button>
-          <button
-            onClick={() => register()}
-            type="button"
-            className="btn text-xl text-white pt-2 bg-transparent border-hidden hover:bg-transparent hover:opacity-75"
-          >
-            Sign Up
-          </button>
-        </div>
-      </nav>
-    );
-  } else {
-    return (
-      <nav
-        className="flex w-full h-24 text-2xl px-5 justify-between items-center"
-        style={{ backgroundColor: '#2485A9', color: '#ffffff' }}
-      >
-        <Link
-          to="/home"
-          className="inline-block border-none hover:bg-transparent p-2 rounded-lg"
-        >
-          <img
-            className="border-none hover:opacity-75"
-            style={{ width: '80px', height: '80px' }}
-            src={'assets/logo.png'}
-            alt="logo"
-          />
-        </Link>
-        <h1 className="text-5xl">BeKindly</h1>
-        <div className="flex items-center">
-          <Link to="/calendar" data-testid="calendar-link">
-            <div className="btn text-5xl text-white pr-4 pt-2 bg-transparent border-hidden hover:bg-transparent hover:opacity-75">
-              <BsCalendar4 />
-            </div>
-          </Link>
-          <Link to="/profile" data-testid="profile-link">
-            <div className="btn text-5xl text-white pt-2 bg-transparent border-hidden hover:bg-transparent hover:opacity-75">
-              <BsPersonCircle />
-            </div>
-          </Link>
-        </div>
-      </nav>
-    );
-  }
-}
+        <img
+          className="border-none hover:opacity-75"
+          style={{ width: '80px', height: '80px' }}
+          src={'assets/logo.png'}
+          alt="logo"
+        />
+      </Link>
+      <h1 className="text-5xl">BeKindly</h1>
+      <div className="flex items-center">
+        {inDev || isAuthenticated ? (
+          <>
+            <Link to="/calendar" data-testid="calendar-link">
+              <div className="btn text-5xl text-white pr-4 pt-2 bg-transparent border-hidden hover:bg-transparent hover:opacity-75">
+                <BsCalendar4 />
+              </div>
+            </Link>
+            <Link to="/profile" data-testid="profile-link">
+              <div className="btn text-5xl text-white pt-2 bg-transparent border-hidden hover:bg-transparent hover:opacity-75">
+                <BsPersonCircle />
+              </div>
+            </Link>
+          </>
+        ) : (
+          <>
+            <button
+              onClick={() => login()}
+              type="button"
+              className="btn text-xl text-white pt-2 bg-transparent border-hidden hover:bg-transparent hover:opacity-75"
+            >
+              Sign In
+            </button>
+            <button
+              onClick={() => register()}
+              type="button"
+              className="btn text-xl text-white pt-2 bg-transparent border-hidden hover:bg-transparent hover:opacity-75"
+            >
+              Sign Up
+            </button>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
