36eaa2f9b57382e7c57114a6d085a024f06095e5
Bryce <greene8616@gmail.com>
2024-04-23 22:22:01 -0500

    fix capitalization of component name

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,35 +1,38 @@
-
-
 interface UserFeed {
-    user: string;
-    profilePic: string;
-    kindness: string;
-
+  user: string;
+  profilePic: string;
+  kindness: string;
 }
 
-function feed() {
+function Feed() {
+  const userfeed: UserFeed = {
+    user: "Tucker.Brown",
+    profilePic:
+      "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
+    kindness: "Did not underestimate Kaylee's power today.",
+  };
 
-    const userfeed: UserFeed = {
-        user: 'Tucker.Brown',
-        profilePic: 'https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg',
-        kindness: "Did not underestimate Kaylee's power today."
-    }
-
-    return(
-            <div className="bg-kindly-offWhite">
-                <div className="chat chat-start bg-kindly-offWhite">
-                <div className="chat-image avatar">
-                    <div className="w-10 rounded-full">
-                    <img alt="Tailwind CSS chat bubble component" src={userfeed.profilePic} />
-                    </div>
-                </div>
-                <div className="chat-header">
-                    {userfeed.user}
-                </div>
-                <div className="chat-bubble shadow-md bg-white" style={{width:'23rem'}}>{userfeed.kindness}</div>
-                </div>
-            </div>
-    );
+  return (
+    <div className="bg-kindly-offWhite">
+      <div className="chat chat-start bg-kindly-offWhite">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS chat bubble component"
+              src={userfeed.profilePic}
+            />
+          </div>
+        </div>
+        <div className="chat-header">{userfeed.user}</div>
+        <div
+          className="chat-bubble shadow-md bg-white"
+          style={{ width: "23rem" }}
+        >
+          {userfeed.kindness}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default feed;
+export default Feed;
