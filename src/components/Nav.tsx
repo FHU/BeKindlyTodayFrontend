b79ef2e62b6ee382a9c2257a05573bb6ad65fa29
kayleeodom <kaylee.odom@students.fhu.edu>
2024-04-23 20:24:16 -0500

    nav work

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <nav
       className="flex w-full h-24 text-2xl px-5 justify-between items-center"
-      style={{ backgroundColor: '#2485A9', color: '#ffffff' }}
+      style={{ backgroundColor: '#1964de', color: '#ffffff' }}
     >
       <Link
         to="/home"
