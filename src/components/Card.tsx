dd5b1aff572b74a701bd284177ef9294bcb414e8
Bryce <greene8616@gmail.com>
2024-04-23 22:24:10 -0500

    refactor card and home page to be more React-ish and to be easier to follow

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,61 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import { BsCheckCircle, BsCheck2, BsCheck2All } from 'react-icons/bs';
-import Feed from '../components/Feed';
-import 'daisyui/dist/full.css';
+import React, { useState, useEffect } from "react";
+import { BsCheckCircle, BsCheck2, BsCheck2All } from "react-icons/bs";
+import Feed from "../components/Feed";
+import "daisyui/dist/full.css";
 
-export let completedChallenge = false;
-let userInput = '';
-interface Post{
+let userInput = "";
+interface Post {
   challenge: string;
   visual: string;
   experience: string;
-};
+}
 
 interface CardProps {
-  layoutType: 'home' | 'completion' | 'confirmation' | 'staticFeed';
+  layoutType: "home" | "completion" | "confirmation" | "staticFeed";
   handleButtonClick: () => void;
+  completedChallenge: boolean;
 }
 
 const Card: React.FC<CardProps> = ({ layoutType, handleButtonClick }) => {
-  const [internalLayoutType, setInternalLayoutType] = useState<
-    'home' | 'completion' | 'confirmation' | 'staticFeed'
-  >(layoutType);
   const [showShadow, setShowShadow] = useState<boolean>(false);
-  const [textValue, setTextValue] = useState('');
+  const [textValue, setTextValue] = useState("");
 
   console.log(showShadow);
 
-  useEffect(() => {
-    if (completedChallenge && internalLayoutType === 'home') {
-      setInternalLayoutType('confirmation');
-    }
-  }, [completedChallenge, internalLayoutType]);
-
   const post: Post = {
-    challenge: 'Send a text to a loved one to show your appreciation.',
-    visual: 'images/phone.jpg',
+    challenge: "Send a text to a loved one to show your appreciation.",
+    visual: "images/phone.jpg",
     experience: userInput,
   };
 
   const handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     const { value } = event.target;
     setTextValue(value.slice(0, 250)); // Limiting to 250 characters
-    console.log('User input:', value); // Logging user input
+    console.log("User input:", value); // Logging user input
     userInput = value; // Exporting user input by assigning it to userInput variable
   };
 
-  const handleButtonClickInternal = () => {
-    completedChallenge = true; // Set completedChallenge to true when button is clicked
-    console.log('completedChallenge is set to true:', completedChallenge); // Log that completedChallenge is true
-    // Call the original handleButtonClick function
-    if (typeof handleButtonClick === 'function') {
-      handleButtonClick();
-    }
-  };
-
   let cardBody;
-  switch (internalLayoutType) {
-    case 'home':
+  switch (layoutType) {
+    case "home":
       cardBody = (
         <div className="card-body">
           <div className="flex flex-row">
@@ -76,7 +58,7 @@ const Card: React.FC<CardProps> = ({ layoutType, handleButtonClick }) => {
           </div>
           <div className="card-actions justify-center pt-4">
             <button
-              onClick={handleButtonClickInternal} // Changed to handleButtonClickInternal
+              onClick={handleButtonClick} // Changed to handleButtonClickInternal
               className="btn btn-block rounded-full text-xl bg-kindly-blue text-white border-none transition-colors duration-300 hover:bg-kindly-royalBlue"
             >
               <div>
@@ -89,60 +71,60 @@ const Card: React.FC<CardProps> = ({ layoutType, handleButtonClick }) => {
       );
       break;
 
-    case 'completion':
+    case "completion":
       const isButtonDisabled = textValue.length === 0; // Check if textValue is empty
       cardBody = (
-      <div className="card-body bg-white rounded-2xl">
-        <div className="flex flex-row">
-        <div className="text-3xl text-kindly-blue pr-2 pt-3">
-          <BsCheck2 />
-        </div>
-        <p className="font-bold text-lg text-kindly-blue">
-          {post.challenge}
-        </p>
-        </div>
-        <div className="flex flex-row ">
-        <div className="text-3xl text-kindly-blue pr-2 pt-1">
-          <BsCheck2All />
-        </div>
-        <p className="font-semibold text-black">
-          Make it a video or audio message instead of a regular text.
-        </p>
-        </div>
-        <div className="card-actions justify-center">
-        <div className="my-6">
-          <h2 className="text-2xl self-start">Experience</h2>
-          <div className="relative">
-          <form action="#" method="post" className="flex flex-col">
-            <textarea
-            className="w-80 border-2 border-black rounded-lg p-3 h-32 bg-kindly-offWhite"
-            value={textValue}
-            onChange={handleTextChange}
-            placeholder="Write your experience here..."
-            ></textarea>
-          </form>
+        <div className="card-body bg-white rounded-2xl">
+          <div className="flex flex-row">
+            <div className="text-3xl text-kindly-blue pr-2 pt-3">
+              <BsCheck2 />
+            </div>
+            <p className="font-bold text-lg text-kindly-blue">
+              {post.challenge}
+            </p>
           </div>
-          <div className="text-right p-1 text-sm">
-          {`${textValue.length} / 250`}
+          <div className="flex flex-row ">
+            <div className="text-3xl text-kindly-blue pr-2 pt-1">
+              <BsCheck2All />
+            </div>
+            <p className="font-semibold text-black">
+              Make it a video or audio message instead of a regular text.
+            </p>
+          </div>
+          <div className="card-actions justify-center">
+            <div className="my-6">
+              <h2 className="text-2xl self-start">Experience</h2>
+              <div className="relative">
+                <form action="#" method="post" className="flex flex-col">
+                  <textarea
+                    className="w-80 border-2 border-black rounded-lg p-3 h-32 bg-kindly-offWhite"
+                    value={textValue}
+                    onChange={handleTextChange}
+                    placeholder="Write your experience here..."
+                  ></textarea>
+                </form>
+              </div>
+              <div className="text-right p-1 text-sm">
+                {`${textValue.length} / 250`}
+              </div>
+            </div>
+            <button
+              onClick={handleButtonClick}
+              className="btn btn-block rounded-full text-xl bg-kindly-blue text-white border-none transition-colors duration-300 hover:bg-kindly-royalBlue"
+              name="completeChallenge"
+              disabled={isButtonDisabled} // Disable the button if textValue is empty
+            >
+              <div>
+                <BsCheckCircle />
+              </div>
+              Complete
+            </button>
           </div>
         </div>
-        <button
-          onClick={handleButtonClickInternal} // Changed to handleButtonClickInternal
-          className="btn btn-block rounded-full text-xl bg-kindly-blue text-white border-none transition-colors duration-300 hover:bg-kindly-royalBlue"
-          name="completeChallenge"
-          disabled={isButtonDisabled} // Disable the button if textValue is empty
-        >
-          <div>
-          <BsCheckCircle />
-          </div>
-          Complete
-        </button>
-        </div>
-      </div>
       );
       break;
 
-    case 'confirmation':
+    case "confirmation":
       cardBody = (
         <div className="card-body">
           <div className="text-black text-center py-6">
@@ -154,15 +136,11 @@ const Card: React.FC<CardProps> = ({ layoutType, handleButtonClick }) => {
       );
       break;
 
-    case 'staticFeed':
-      cardBody = <Feed />;
-      break;
-
     default:
       cardBody = null;
   }
 
-  const image = internalLayoutType !== 'staticFeed' && (
+  const image = (
     <figure className="rounded-none">
       <img src={post.visual} alt="Challenges" />
     </figure>
@@ -171,7 +149,7 @@ const Card: React.FC<CardProps> = ({ layoutType, handleButtonClick }) => {
   return (
     <div
       className={`card card-compact w-96 bg-base-100 bg-white ${
-        layoutType === 'completion' ? 'custom-class' : ''
+        layoutType === "completion" ? "custom-class" : ""
       }`}
       onMouseEnter={() => setShowShadow(true)}
       onMouseLeave={() => setShowShadow(false)}
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,81 +1,105 @@
-import React, { useState, useEffect } from 'react';
-import Navbar from '../components/Nav';
-import Stats from '../components/Stats';
-import Footer from '../components/Footer';
-import CountdownTimer from '../components/Timer';
-import Card from '../components/Card';
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/Nav";
+import Stats from "../components/Stats";
+import Footer from "../components/Footer";
+import CountdownTimer from "../components/Timer";
+import Card from "../components/Card";
+import Feed from "../components/Feed";
+import { getHasCompleted } from "../services/CompletionService";
 
-// Importing the completedChallenge variable
-import { completedChallenge } from '../components/Card'; // Importing completedChallenge variable
-
-// import { userInput } from '../components/Card'; 
-
-
-
+// import { userInput } from '../components/Card';
 
 const Home: React.FC = () => {
-    const [currentPage, setCurrentPage] = useState<'home' | 'completion' | 'confirmation'>('home');
+  const [currentPage, setCurrentPage] = useState<
+    "home" | "completion" | "confirmation"
+  >();
+  const [completedChallenge, setCompletedChallenge] = useState<boolean>(false);
 
-    useEffect(() => {
-        // Check if completedChallenge is true and set currentPage accordingly
-        if (completedChallenge) {
-            setCurrentPage('confirmation');
-        }
-    }, []); // Empty dependency array ensures useEffect runs only once
+  // Set completedChallenge using the getHasCompleted Service
+  useEffect(() => {
+    (async () => {
+      const completion_response = await getHasCompleted();
+      setCompletedChallenge(completion_response.completed);
+    })();
+  }, []);
 
-    const handleCompletionButtonClick = () => {
-        setCurrentPage('completion');
-    };
+  // Update current page based on completedChallenge
+  useEffect(() => {
+    const page = completedChallenge ? "confirmation" : "home";
+    setCurrentPage(page);
+  }, [completedChallenge]);
 
-    const handleConfirmationButtonClick = () => {
-        setCurrentPage('confirmation');
-    };
+  const handleConfirmationButtonClick = () => {
+    setCurrentPage("confirmation");
+  };
 
-    return (
-        <div className='flex flex-col gap-y-10 items-center bg-kindly-offWhite'>
-            <Navbar/>
+  const handleOnClick = () => {
+    if (currentPage === "home") {
+      setCurrentPage("completion");
+    } else if (currentPage === "completion") {
+      setCompletedChallenge(true);
+    }
+  };
 
-            {/* Conditional Rendering based on currentPage */}
-            {currentPage === 'home' && (
-                <div>
-                    {/* Stats Section */}
-                    <div className='flex justify-center pb-6'><Stats /></div>
-                    <CountdownTimer />
-                    <h2 className="text-3xl py-3 text-white text-center font-extrabold bg-kindly-blue rounded-t-2xl">
-                        Today's Challenge
-                    </h2>
-                    <Card layoutType='home' handleButtonClick={handleCompletionButtonClick} />
-                </div>
-            )}
-            {currentPage === 'completion' && (
-                <div className="flex flex-col gap-y-10 items-center text-black bg-kindly-offWhite">
-                    <div className="flex flex-col items-center gap-4 pb-10 bg-kindly-backgroundColor">
-                        {/* Stats Section */}
-                        <CountdownTimer />
-                        <div>
-                            <h2 className="text-3xl py-3 text-white text-center font-extrabold bg-kindly-blue rounded-t-2xl">
-                                Today's Challenge
-                            </h2>
-                            <Card layoutType="completion" handleButtonClick={handleConfirmationButtonClick} />
-                        </div>
-                    </div>
-                </div>
-            )}
-            {currentPage === 'confirmation' && (
-                <div className="flex flex-col gap-y-10 items-center pb-10 bg-kindly-offWhite">
-                    <Stats />
-                    <div>
-                        <h2 className="text-3xl py-3 text-white text-center font-extrabold bg-kindly-blue rounded-t-2xl">
-                            Completed Challenge
-                        </h2>
-                        <Card layoutType="confirmation" handleButtonClick={handleConfirmationButtonClick} />
-                    </div>
-                    <Card layoutType='staticFeed' handleButtonClick={handleConfirmationButtonClick}/>
-                </div>
-            )}
-            <Footer/>
+  return (
+    <div className="flex flex-col gap-y-10 items-center bg-kindly-offWhite">
+      <Navbar />
+
+      {/* Conditional Rendering based on currentPage */}
+      {currentPage === "home" && (
+        <div>
+          {/* Stats Section */}
+          <div className="flex justify-center pb-6">
+            <Stats />
+          </div>
+          <CountdownTimer />
+          <h2 className="text-3xl py-3 text-white text-center font-extrabold bg-kindly-blue rounded-t-2xl">
+            Today's Challenge
+          </h2>
+          <Card
+            layoutType={currentPage}
+            handleButtonClick={handleOnClick}
+            completedChallenge={completedChallenge}
+          />
         </div>
-    );
+      )}
+      {currentPage === "completion" && (
+        <div className="flex flex-col gap-y-10 items-center text-black bg-kindly-offWhite">
+          <div className="flex flex-col items-center gap-4 pb-10 bg-kindly-backgroundColor">
+            {/* Stats Section */}
+            <CountdownTimer />
+            <div>
+              <h2 className="text-3xl py-3 text-white text-center font-extrabold bg-kindly-blue rounded-t-2xl">
+                Today's Challenge
+              </h2>
+              <Card
+                layoutType="completion"
+                handleButtonClick={handleOnClick}
+                completedChallenge={completedChallenge}
+              />
+            </div>
+          </div>
+        </div>
+      )}
+      {currentPage === "confirmation" && (
+        <div className="flex flex-col gap-y-10 items-center pb-10 bg-kindly-offWhite">
+          <Stats />
+          <div>
+            <h2 className="text-3xl py-3 text-white text-center font-extrabold bg-kindly-blue rounded-t-2xl">
+              Completed Challenge
+            </h2>
+            <Card
+              layoutType="confirmation"
+              handleButtonClick={handleConfirmationButtonClick}
+              completedChallenge={completedChallenge}
+            />
+          </div>
+          <Feed />
+        </div>
+      )}
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
