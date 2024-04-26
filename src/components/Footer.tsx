42c16e33e51c933ffba2f126bb0365e4780a9443
Priscilla Harris <89226780+PHarris2002@users.noreply.github.com>
2024-04-26 13:58:27 -0500

    Progress Footer Responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { FaRegCopyright } from "react-icons/fa6";
 function footer() {
 
     return(
-        <footer className="flex flex-row justify-center w-full bg-kindly-blue text-kindly-offWhite space-x-4 p-5 mt-8">
+        <footer className="flex flex-row text-xs md:text-base justify-center items-center w-full bg-kindly-blue text-kindly-offWhite space-x-4 p-5 mt-8">
             <FaRegCopyright className='opacity-80' />
             <p className='opacity-80'>2024 CIS467</p>
             <div className='flex items-end space-x-2'>
