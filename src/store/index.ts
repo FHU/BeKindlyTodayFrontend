2b9cab9e3d14cccef4e220f23601a379310c134a
Bryce <greene8616@gmail.com>
2024-04-26 01:04:01 -0500

    import work from previous project and modify for Bekindly

---
+++ src/store/index.ts
@@ -0,0 +1,44 @@
+// index from https://dev.to/koladev/django-rest-authentication-cmh
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import authSlice from "./slices/auth";
+
+const rootReducer = combineReducers({
+  auth: authSlice.reducer,
+});
+
+const persistedReducer = persistReducer(
+  {
+    key: "root",
+    version: 1,
+    storage: storage,
+  },
+  rootReducer
+);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
+});
+
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof rootReducer>;
+
+export default store;
