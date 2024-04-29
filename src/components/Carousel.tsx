8907db96221505c6bfe8404b2d37aff1f4b53146
Aapple21 <aapplegate12@icloud.com>
2024-04-29 15:06:48 -0500

    fix card issue #1

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,4 +1,3 @@
-// Carousel.tsx
 import React from "react";
 import { ProfilePicture } from "../views/Profile";
 
@@ -13,21 +12,20 @@ const Carousel: React.FC<CarouselProps> = ({
 }) => {
   return (
     <div className="carousel carousel-center rounded-box flex gap-2 overflow-x-auto">
-  {profilePictures.map((profilePicture, index) => (
-    <div
-      className="carousel-item rounded-full bg-white w-40 h-40 flex items-center justify-center cursor-pointer"
-      key={index}
-      onClick={() => onProfilePictureChange(profilePicture.path)}
-    >
-      <img
-        src={profilePicture.path}
-        alt={profilePicture.name}
-        className="rounded-full w-full h-full"
-      />
+      {profilePictures.map((profilePicture, index) => (
+        <div
+          className="carousel-item rounded-full bg-white w-40 h-40 flex items-center justify-center cursor-pointer"
+          key={index}
+          onClick={() => onProfilePictureChange(profilePicture.path)}
+        >
+          <img
+            src={profilePicture.path}
+            alt={profilePicture.name}
+            className="rounded-full w-full h-full"
+          />
+        </div>
+      ))}
     </div>
-  ))}
-</div>
-
   );
 };
 
