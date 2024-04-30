7a0143efd7ff1e2eee0648e73f663a5d97620a3f
Aapple21 <aapplegate12@icloud.com>
2024-04-30 18:59:50 -0500

    card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -177,7 +177,7 @@ const Card: React.FC<CardProps> = ({
 
   return (
     <div
-      className={`card card-compact sm:w-96 w-80 bg-base-100 bg-white ${
+      className={`card card-compact sm:w-96 w-80 bg-base-100 rounded-t-xl bg-white ${
         layoutType === "completion" ? "custom-class" : ""
       }`}
     >
