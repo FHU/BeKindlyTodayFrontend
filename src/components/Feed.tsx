9b92cf1f93c2c9ca92c550326e7a5a125060d341
kayleeodom <kaylee.odom@students.fhu.edu>
2024-04-29 23:26:45 -0500

    trying something

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -10,7 +10,7 @@ const Feed = ({ completion }: Props) => {
 
   return (
     <div className="place-items-center">
-      <div className="chat chat-start bg-kindly-offWhite sm:w-96">
+      <div className="chat chat-start bg-kindly-offWhite sm:w-fit">
         <div className="chat-image avatar">
           <div className="w-10 sm:w-7  rounded-full">
             <img
