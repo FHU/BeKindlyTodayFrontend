eafe45f38b332542d97020c5b17075fabd5d43d2
kayleeodom <kaylee.odom@students.fhu.edu>
2024-04-29 22:46:04 -0500

    feed work

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -10,7 +10,7 @@ const Feed = ({ completion }: Props) => {
 
   return (
     <div className="">
-      <div className="chat chat-start bg-kindly-offWhite w-96">
+      <div className="chat chat-start bg-kindly-offWhite">
         <div className="chat-image avatar">
           <div className="w-10 sm:w-7  rounded-full">
             <img
