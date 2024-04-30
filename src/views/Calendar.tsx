cf550d7be855de3ca277ffcc5ed0d1387a1cd23a
Aapple21 <aapplegate12@icloud.com>
2024-04-30 10:07:19 -0500

    working on today #3

--- src/views/Calendar.tsx
+++ src/views/Calendar.tsx
@@ -238,7 +238,7 @@ const CalendarPage = () => {
                 })}
               />
             </div>
-            {currentMonth !== new Date().getMonth() && currentMonth !== new Date().getFullYear() && (
+            {currentMonth !== new Date().getMonth() && currentYear !== new Date().getFullYear() && (
               <button
                 className="bg-kindly-blue text-white p-1 rounded-md"
                 onClick={goToToday}
