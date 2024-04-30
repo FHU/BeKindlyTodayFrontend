dba5b83e58cd87f04fc00463c019b01e0f72ed61
Aapple21 <aapplegate12@icloud.com>
2024-04-30 12:05:48 -0500

    annoyed

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -101,7 +101,7 @@ const Card: React.FC<CardProps> = ({
       case "completion":
         const isButtonDisabled = textValue.length === 0; // Check if textValue is empty
         cardBody = (
-          <div className="card-body bg-white">
+          <div className="card-body bg-white rounded-xl">
             <div className="flex flex-row items-center">
               <div className="text-3xl text-kindly-blue pr-2">
                 <BsCheck2 />
