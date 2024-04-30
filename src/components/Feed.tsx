a0d5d8b8fdee2dbf2cb3abf5c909ffc394d2ba74
kayleeodom <kaylee.odom@students.fhu.edu>
2024-04-30 10:10:46 -0500

    feed work

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -9,20 +9,25 @@ const Feed = ({ completion }: Props) => {
     completion.user.profilePicture || "images/Default_Profile.png";
 
   return (
-    <div className="chat chat-start bg-kindly-offWhite place-items-center w-96">
-      <div className="chat-image avatar">
-        <div className="w-10 sm:w-7  rounded-full">
-          <img
-            alt="Tailwind CSS chat bubble component"
-            src={user_profile_picture}
-          />
+    <div className="place-items-center w-96">
+      <div className="chat chat-start bg-kindly-offWhite">
+        {/* avatar image */}
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS chat bubble component"
+              src={user_profile_picture}
+            />
+          </div>
         </div>
-      </div>
-      <div className="chat-header text-black">{completion.user.username}</div>
-      <div
-        className="chat-bubble shadow-md bg-white text-black"
-      >
-        {completion.description}
+        {/* username */}
+        <div className="chat-header text-black">{completion.user.username}</div>
+        {/* chat bubble */}
+        <div
+          className="chat-bubble shadow-md sm:w-72 bg-white text-black"
+        >
+          {completion.description}
+        </div>
       </div>
     </div>
   );
