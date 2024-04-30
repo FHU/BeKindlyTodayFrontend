ab5d92212c9744c1728140e3774fc714d080332d
kayleeodom <kaylee.odom@students.fhu.edu>
2024-04-29 22:26:16 -0500

    feed wrk

--- src/views/Calendar.tsx
+++ src/views/Calendar.tsx
@@ -163,13 +163,13 @@ const CalendarPage = () => {
       style={{ maxWidth: "800px" }}
     >
       <div className="flex flex-row">
-        <div className="flex flex-col">
+        <div className="flex flex-col pr-2">
           <div className="text-2xl pb-4">
             {userStreak}&nbsp; {/* Non-breaking space */}
           </div>
-          <div className="text-lg">Day Streak!</div>
+          <div className="text-2xl">Day Streak!</div>
         </div>
-        <div className="text-white justify-center text-5xl"><BsFire/></div>
+        <div className="text-white justify-center text-7xl"><BsFire/></div>
       </div>
     </div>
   );
