77ff3bf4aff432d29143afe1ef2b50df1d213626
kayleeodom <kaylee.odom@students.fhu.edu>
2024-04-30 22:22:14 -0500

    card work

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -34,7 +34,7 @@ const Card: React.FC<CardProps> = ({
   switch (layoutType) {
     case "home":
       cardBody = (
-        <div className="card-body bg-white">
+        <div className="card-body bg-white rounded-b-xlcard">
           <div className="flex flex-row items-center">
             <div className="text-3xl text-kindly-blue pr-2">
               <BsCheck2 />
