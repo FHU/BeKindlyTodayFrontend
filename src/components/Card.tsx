e0417040e63c8c8095a64ca996ff17011b8ffbe2
kayleeodom <kaylee.odom@students.fhu.edu>
2024-04-30 22:27:05 -0500

    feed work

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -153,7 +153,7 @@ const Card: React.FC<CardProps> = ({
 
     case "confirmation":
       cardBody = (
-        <div className="card-body bg-white rounded-b-xl"> 
+        <div className="card card-body bg-white"> 
           <div className="text-black text-center py-6">
             <p className="text-lg font-semibold leading-tight">
               {completion?.description}
